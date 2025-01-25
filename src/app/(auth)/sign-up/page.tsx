4c563376672040397daf71bfd6e3d5de67899fc7
DebasishDas1 <debasishdas10000000@gmail.com>
2025-01-25 17:53:36 +0530

    commit SignupForm

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 import { buttonVariants } from "@/components/ui/button";
-import SignUpForm from "@/components/SignUpForm";
+import SignUpForm from "@/components/SignupForm";
 
 const SignUp = async () => {
   return (
